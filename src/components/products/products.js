c95a744335aed63c5f90308138ded9e8b6e3bb04
mizza-cz <mizza.cz666@gmail.com>
2025-05-27 07:46:18 +0200

    add hp

---
+++ src/components/products/products.js
@@ -0,0 +1,68 @@
+(function () {
+  const masonryGrid = document.querySelector(".masonry-products-grid");
+
+  if (masonryGrid) {
+    // Init masonry
+    const msnry = new Masonry(masonryGrid, {
+      itemSelector: ".masonry-products-grid__item",
+      gutter: 28,
+      horizontalOrder: false,
+    });
+
+    const loadMoreBtn = document.querySelector(".products-load-more");
+    if (!msnry || !loadMoreBtn || typeof masonryReferenceSource === "undefined")
+      return;
+
+    loadMoreBtn.addEventListener("click", async () => {
+      const loader = document.createElement("div");
+      loader.classList.add("loader");
+      loadMoreBtn.prepend(loader);
+
+      const productsContainer = document.querySelector(".products");
+      if (productsContainer) {
+        productsContainer.style.maxHeight = "none";
+        productsContainer.style.overflow = "visible";
+      }
+
+      const url = new URL(window.location.origin + masonryReferenceSource);
+      const page = loadMoreBtn.dataset.page ?? 2;
+      url.searchParams.append("page", page);
+
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: { Accept: "application/json" },
+        });
+
+        if (!response.ok) {
+          loader.remove();
+          return;
+        }
+
+        const data = await response.json();
+
+        if (data.html) {
+          const range = document.createRange();
+          const documentFragment = range.createContextualFragment(data.html);
+          const elements = Array.from(documentFragment.children);
+
+          elements.forEach((element) => {
+            masonryGrid.append(element);
+          });
+
+          msnry.appended(elements);
+        }
+
+        if (data.loadMore === false) {
+          loadMoreBtn.style.display = "none";
+        } else {
+          loadMoreBtn.dataset.page = parseInt(page) + 1;
+        }
+      } catch (error) {
+        console.error("error", error);
+      } finally {
+        loader.remove();
+      }
+    });
+  }
+})();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,10 @@
 //=include ../../node_modules/vanilla-lazyload/dist/lazyload.js
 //=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
-
+//=include ../../node_modules/select2/dist/js/select2.min.js
 //=include ../../node_modules/swiper/swiper.js
 //=include ../../node_modules/swiper/swiper-bundle.min.js
+//=include ../../node_modules/masonry-layout/dist/masonry.pkgd.min.js
+//=include ../../node_modules/masonry-layout/masonry.js
 document.addEventListener("DOMContentLoaded", function () {
   //=include ../components/**/*.js
   //=include custom/*.js
