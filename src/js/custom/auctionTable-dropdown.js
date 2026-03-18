143187bac0eef3554beb228f662e6dd06227550a
mizza-cz <mizza.cz666@gmail.com>
2026-03-18 15:23:35 +0100

    add fixes

---
+++ src/js/custom/auctionTable-dropdown.js
@@ -0,0 +1,45 @@
+(function () {
+  if (document.readyState === "loading") {
+    document.addEventListener("readystatechange", init);
+  } else {
+    init();
+  }
+
+  function init() {
+    const buttons = document.querySelectorAll(".auctionHistory__btn");
+
+    if (!buttons.length) return;
+
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const currentRow = btn.closest("tr");
+        const detailsRow = currentRow.nextElementSibling;
+
+        if (
+          !detailsRow ||
+          !detailsRow.classList.contains("auction-history-details")
+        ) {
+          return;
+        }
+
+        document.querySelectorAll(".auction-history-details").forEach((row) => {
+          if (row !== detailsRow) {
+            row.classList.add("d-none");
+
+            const parentTr = row.previousElementSibling;
+            if (parentTr) {
+              parentTr.classList.remove("active-row");
+              const btnInRow = parentTr.querySelector(".auctionHistory__btn");
+              if (btnInRow) btnInRow.classList.remove("active");
+            }
+          }
+        });
+
+        const isHidden = detailsRow.classList.contains("d-none");
+        detailsRow.classList.toggle("d-none", !isHidden);
+        btn.classList.toggle("active", isHidden);
+        currentRow.classList.toggle("active-row", isHidden);
+      });
+    });
+  }
+})();
